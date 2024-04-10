d5c996187fe3e96c566b41c76f7b361129271704
kjone8371 <kjone8371@gmail.com>
2024-04-10 13:31:47 +0900

    7주차

--- src/components/include/Section_05.js
+++ src/components/include/Section_05.js
@@ -1,36 +1,12 @@
-import { useEffect, useState } from "react";
 
+import "../../assets/css/section/section_05.css";
 
 const Section_05 = () => {
-    const [scrollY, setScrollY] = useState(0);
-    const [circles, setCircles] = useState([]);
-  
-    useEffect(() => {
-      const handleScroll = () => {
-        setScrollY(window.scrollY);
-      };
-  
-      window.addEventListener('scroll', handleScroll);
-  
-      return () => {
-        window.removeEventListener('scroll', handleScroll);
-      };
-    }, []);
-  
-    useEffect(() => {
-      // 스크롤 위치가 30 이상일 때만 원을 생성합니다.
-      if (scrollY >= 30) {
-        const newCircle = { id: circles.length, x: 50, y: scrollY};
-        setCircles([...circles, newCircle]);
-      }
-    }, [scrollY]);
   
     return (
-      <svg width="100%" height="100%">
-        {circles.map(circle => (
-          <circle key={circle.id} cx={circle.x} cy={circle.y} r="10" fill="red" />
-        ))}
-      </svg>
+     <section id="experience" className="section5">
+      dwa
+      </section>
     );
   };
 
--- src/components/include/Section_06.js
+++ src/components/include/Section_06.js
@@ -1,5 +1,6 @@
 import "../../assets/css/section/section_06.css";
-
+import Winter from "../../assets/image/winter.png";
+import Winter1 from "../../assets/image/winter1.png"
 
 
 const Section_06 = () => {
@@ -7,8 +8,15 @@ const Section_06 = () => {
         <section className="section6">
             <div className="finshed">
                 <h1>연락주세요:)</h1>
-                <em></em>
+                <div className="my_info">
+                    <a href="mailto:rlawjddnsj8371@gmail.com" className="mail">메일</a>
+                    <a href="https://github.com/kjone8371" className="github">깃허브</a>
+                    <a href="tel:010-5238-8371" className="phone">전화</a>
+                </div>
             </div>
+            <img className="Winter" src={Winter} alt="Winter"/>
+            <img className="winter1" src={Winter1} alt="Winter1"/>
+
         </section>
     );
 }
